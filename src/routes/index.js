8b247cd0c476bcd35a7e0d172131f4c04d8c24e2
Abhijit Utkarsh <abhijitu18@gmail.com>
2022-06-04 20:32:32 +0530

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,11 @@
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
+import { addViewToListingRoute } from "./addViewToListing";
+import { getUserListingsRoute } from "./getUserListings";
+
 export default [
+    addViewToListingRoute,
     getAllListingsRoute,
     getListingRoute,
+    getUserListingsRoute,
 ];
